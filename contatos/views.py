69fc63ef79f38685493c0cf3f82e6824eacdbddc
Leandro <leandrodsilvalara@yahoo.com.br>
2020-02-17 16:56:43 -0300

    BKP

--- contatos/views.py
+++ contatos/views.py
@@ -8,9 +8,10 @@ from django.contrib import messages
 
 
 def index(request):
-    contatos = Contato.objects.order_by('id')
-
-    paginator = Paginator(contatos, 10)
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 20)
 
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -21,7 +22,6 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
-    # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
 
     if not contato.mostrar:
@@ -30,30 +30,32 @@ def ver_contato(request, contato_id):
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
-    
+
+
 def busca(request):
-        termo = request.GET.get('termo')
-        
-        if termo is None or not termo:
-            messages.add_message(
-                request,
-                messages.ERROR,
-                'Campo termo não pode ficar vazio.'
-            )
-            return redirect('index')
-        
-        campos = Concat('nome', Value(' '), 'sobrenome')
-        contatos = Contato.objects.annotate(
-            nome_completo=campos
-        ).filter(
-            Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Campo termo não pode ficar vazio.'
         )
- 
-        paginator = Paginator(contatos, 20)
+        return redirect('index')
 
-        page = request.GET.get('p')
-        contatos = paginator.get_page(page)
+    campos = Concat('nome', Value(' '), 'sobrenome')
 
-        return render(request, 'contatos/busca.html', {
-            'contatos': contatos
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 20)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
